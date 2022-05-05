961ab6fc380f48fd402331a7ade18b1d53075a90
Julian Pagtama <jpagtama@gmail.com>
2022-05-05 07:32:45 -0700

    Adds Expenses.css

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,14 @@
 import ExpenseItem from './ExpenseItem'
+import './Expenses.css'
 
 function Expenses(props) {
+    let expenses = props.expenses.map((element) => {
+        return (
+            <ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date} />
+        )
+    })
     
-    return (
-        props.expenses.map((element) => {
-            return (
-                <ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date} />
-            )
-        })
-    )
+    return <div className='expenses'>{expenses}</div>
 }
 
 export default Expenses
