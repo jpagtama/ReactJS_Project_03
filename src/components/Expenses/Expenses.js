c9e458b0509cb29b907e7e425610d6ecedc9c7e5
Julian Pagtama <jpagtama@gmail.com>
2022-05-05 10:20:00 -0700

    Changes components to use arrow functions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
 
-function Expenses(props) {
+const Expenses = (props) => {
     let expenses = props.expenses.map((element) => {
         return (
             <ExpenseItem key={element.id} title={element.title} amount={element.amount} date={element.date} />
