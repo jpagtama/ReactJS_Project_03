8dcce9286af7ed560032afc6620c33de2a9fd90b
Julian Pagtama <jpagtama@gmail.com>
2022-05-23 14:42:24 -0700

    Adds Button component

---
+++ src/components/UI/Button.js
@@ -0,0 +1,11 @@
+import buttonStyles from './Button.module.css'
+
+const Button = props => {
+    console.log("Props.disabled: ", props)
+    return (
+        <button className={`${buttonStyles.button} ${props.className}`} type={props.type || "button"} onClick={props.onClick} disabled={props.isDisabled} >
+            {props.children}
+        </button>
+    )
+}
+export default Button
